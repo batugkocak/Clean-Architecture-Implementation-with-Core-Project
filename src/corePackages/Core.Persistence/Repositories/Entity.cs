6043235c56732d56d63eb46d97c23e069660ac9b
Batug Kocak <batugkocak@gmail.com>
2024-08-22 14:05:45 +0300

    add project files

---
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,22 @@
+namespace Core.Persistence.Repositories;
+
+public class Entity<TId> 
+{
+    public TId Id { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime? UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public Entity()
+    {
+        Id = default;
+    }
+    
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
---
+++ src/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid> 
+{
+    public string Name { get; set; }
+
+    public Brand()
+    {
+        
+    }
+
+    public Brand(Guid id, string name) : base(id)
+    {
+        Name = name;
+    }
+}
