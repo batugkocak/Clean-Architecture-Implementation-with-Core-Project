51140da4242ea935a7267174819e3bcaf52bb5e6
Batug Kocak <batugkocak@gmail.com>
2024-08-27 16:43:58 +0300

    feat: Add GetListByDynamic Query to Model domain with Dynamic implementation bugfix at DB

--- src/rentACar/Application/Features/Models/Profiles/MappingProfiles.cs
+++ src/rentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -1,5 +1,6 @@
 using Application.Features.Brands.Queries.GetList;
 using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using AutoMapper;
 using Core.Application.Responses;
 using Core.Persistence.Paging;
@@ -11,7 +12,17 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
-        CreateMap<Model, GetListModelDto>().ReverseMap();
+        CreateMap<Model, GetListModelDto>()
+            .ForMember(destinationMember: c => c.BrandName, memberOptions: opt => opt.MapFrom(c => c.Brand.Name))
+            .ForMember(destinationMember: c => c.FuelName, memberOptions: opt => opt.MapFrom(c => c.Fuel.Name))
+            .ForMember(destinationMember: c => c.TransmissionName, memberOptions: opt => opt.MapFrom(c => c.Transmission.Name))
+            .ReverseMap();
+        CreateMap<Model, GetListByDynamicModelDto>()
+            .ForMember(destinationMember: c => c.BrandName, memberOptions: opt => opt.MapFrom(c => c.Brand.Name))
+            .ForMember(destinationMember: c => c.FuelName, memberOptions: opt => opt.MapFrom(c => c.Fuel.Name))
+            .ForMember(destinationMember: c => c.TransmissionName, memberOptions: opt => opt.MapFrom(c => c.Transmission.Name))
+            .ReverseMap();
         CreateMap<Paginate<Model>, GetListResponse<GetListModelDto>>().ReverseMap();
+        CreateMap<Paginate<Model>, GetListResponse<GetListByDynamicModelDto>>().ReverseMap();
     }
 } 
--- src/rentACar/WebAPI/Controllers/ModelsController.cs
+++ src/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -1,6 +1,8 @@
 using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using Core.Application.Requests;
 using Core.Application.Responses;
+using Core.Persistence.Dynamic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -15,6 +17,14 @@ namespace WebAPI.Controllers
         {
             GetListModelQuery getListBrandQuery = new() { PageRequest = pageRequest };
             GetListResponse<GetListModelDto> response = await Mediator.Send(getListBrandQuery);
+            return Ok(response);
+        }
+        // GET: api/<ModelsController>
+        [HttpPost("ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] DynamicQuery dynamicQuery)
+        {
+            GetListByDynamicModelQuery getListBrandByDynamicQuery = new() { PageRequest = pageRequest, DynamicQuery = dynamicQuery};
+            GetListResponse<GetListByDynamicModelDto> response = await Mediator.Send(getListBrandByDynamicQuery);
             return Ok(response);
         }
     }
