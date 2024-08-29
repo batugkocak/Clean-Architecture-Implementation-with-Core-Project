953fa6ff7fe5ce0240fc735d7a369a1144d288ea
Batug Kocak <batugkocak@gmail.com>
2024-08-29 09:33:52 +0300

    feat(Core): Implement Validation Pipeline

--- src/corePackages/Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
+++ src/corePackages/Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -7,10 +7,14 @@ public abstract class ExceptionHandler
     public Task HandleExceptionAsync(Exception exception) => exception switch
     {
         BusinessException businessException => HandleExceptionAsync(businessException),
+        ValidationException validationException => HandleExceptionAsync(validationException),
         _ => HandleExceptionAsync(exception)
+        
     };
     
     protected abstract Task HandleExceptionAsync(BusinessException businessException);
+    
+    protected abstract Task HandleExceptionAsync(ValidationException validationException);
     
     protected abstract Task HandleException(Exception exception);
 }
--- src/corePackages/Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
+++ src/corePackages/Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
@@ -2,6 +2,8 @@ using Core.CrossCuttingConcerns.Exceptions.Extensions;
 using Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
 using Core.CrossCuttingConcerns.Exceptions.Types;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ValidationProblemDetails = Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails.ValidationProblemDetails;
 
 namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
 
@@ -17,6 +19,13 @@ public class HttpExceptionHandler : ExceptionHandler
     {
         Response.StatusCode = StatusCodes.Status400BadRequest;
         string details =  new BusinessProblemDetails(businessException.Message).AsJson();
+        return Response.WriteAsync(details);
+    }
+
+    protected override Task HandleExceptionAsync(ValidationException validationException)
+    {
+        Response.StatusCode = StatusCodes.Status400BadRequest;
+        string details =  new ValidationProblemDetails(validationException.Errors).AsJson();
         return Response.WriteAsync(details);
     }
 
