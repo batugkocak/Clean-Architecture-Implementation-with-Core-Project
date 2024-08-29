a7f5639417ad6f0416ea2897e68b9699550f66e8
Batug Kocak <batugkocak@gmail.com>
2024-08-29 10:32:57 +0300

    feat: Add Transaction for CreateBrandCommand

--- src/rentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ src/rentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,12 +1,13 @@
 using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Transaction;
 using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Brands.Commands.Create;
 
-public class CreateBrandCommand : IRequest<CreateBrandResponse>
+public class CreateBrandCommand : IRequest<CreateBrandResponse>, ITransactionalRequest
 {
     public string Name { get; set; }
 
