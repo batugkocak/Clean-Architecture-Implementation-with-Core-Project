6bc68695ae308e61d484c6cf2f69c5475b239b66
Batug Kocak <batugkocak@gmail.com>
2024-09-03 09:05:39 +0300

    feat(Core): Implement Exception Logging

--- src/corePackages/Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
+++ src/corePackages/Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -1,5 +1,10 @@
+using System.Text.Json;
 using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Serilog;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Serilog.Debugging;
 
 namespace Core.CrossCuttingConcerns.Exceptions;
 
@@ -7,11 +12,15 @@ public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly HttpExceptionHandler _httpExceptionHandler;
+    private readonly HttpContextAccessor _httpContextAccessor;
+    private readonly LoggerServiceBase _loggerServiceBase;
 
-    public ExceptionMiddleware(RequestDelegate next)
+    public ExceptionMiddleware(RequestDelegate next, HttpContextAccessor httpContextAccessor, LoggerServiceBase loggerServiceBase)
     {
         _next = next;
         _httpExceptionHandler = new HttpExceptionHandler();
+        _httpContextAccessor = httpContextAccessor;
+        _loggerServiceBase = loggerServiceBase;
     }
 
     public async Task Invoke(HttpContext context)
@@ -22,10 +31,34 @@ public class ExceptionMiddleware
         }
         catch (Exception exception)
         {
+            await LogException(context, exception);
             await HandleExceptionAsync(context.Response, exception);
         }
     }
-    
+
+    private  Task LogException(HttpContext context, Exception exception)
+    {
+        List<LogParameter> logParameters = new()
+        {
+            new LogParameter()
+            {
+                Type = context.GetType().Name, Value = exception.ToString()
+            },
+        };
+
+        LogDetailWithException logDetail = new()
+        {
+            ExceptionMessage = exception.Message,
+            MethodName = _next.Method.Name,
+            Parameters = logParameters,
+            User = _httpContextAccessor.HttpContext?.User.Identity?.Name ?? "?",
+        };
+        
+        _loggerServiceBase.Error(JsonSerializer.Serialize(logDetail));
+
+        return Task.CompletedTask;
+    }
+
     private Task HandleExceptionAsync(HttpResponse response, Exception exception)
     {
         response.ContentType =  "application/json";
